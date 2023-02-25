02dbf2d75c75cbcf6711fc77d212343f0d0e0e4d
Doan325 <124702317+Doan325@users.noreply.github.com>
2023-02-25 17:37:21 -0600

    Add files via upload

---
+++ EVA_1_5_CIRCULO/src/main/java/com/mycompany/eva_1_5_circulo/EVA_1_5_CIRCULO.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_5_circulo;
+import java.util.Scanner;
+
+ //@author Doan Escobar
+ 
+public class EVA_1_5_CIRCULO {
+
+    public static void main(String[] args) {
+Scanner circulo=new Scanner (System.in);
+double radio,perimetro,area;
+System.out.println("ingrese la radio del circulo");
+radio=circulo.nextDouble();
+perimetro=radio*2;
+area=(3.1416)*(radio*radio);
+        System.out.println("el perimetro del circulo es:"+perimetro);
+        System.out.println("el area circulo es:"+area);
+        
+ 
+
+
+
+        
+    
+    
+    
+    }
+}
